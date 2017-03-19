dc58dea7cbc77b6017edcd3c8b52da8fabd25d9a
James Quigley <jmquigley@outlook.com>
2017-03-19 13:01:57 -0500

    Fixes path expansion on Windows
    A statement such as:
    
        let tmp = home(path.join('~/', '.tmp', '.nyc_output'));
    
    On Windows will result in a path that looks like:
    
        '~\tmp\.nyc_output'
    
    Using path.join() will change the path separator in the string, so the code fails to expand using the `USERPROFILE` directory.
    
    The existing code only accepts a string where the first two characters are `~/`.  This change will search for both `~/` and `~\` for the home expansion.  It also changes the test runner to ava (prova didn't work for me on Windows).

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ module.exports = expandHomeDir;
 function expandHomeDir (path) {
   if (!path) return path;
   if (path == '~') return homedir;
-  if (path.slice(0, 2) != '~/') return path;
+  if (path.slice(0, 2) !== '~/' && path.slice(0, 2) !== '~\\') return path;
   return join(homedir, path.slice(2));
 }
